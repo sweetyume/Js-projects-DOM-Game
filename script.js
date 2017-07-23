dc7589ce0691e1cad790956e5ab6e97db8c5342c
sweetyume <sweet.tsukiyume@gmail.com>
2017-07-23 23:21:46 +0200

    script.js roll dice function

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
-
-function gamePlay() {
+var dice =  document.getElementById('dice')
+function rollDice() {
     currentScore ++;
     var dice = Math.floor(Math.random()*6+1);
-    var dice2 = Math.floor(Math.random()*6+1);
     var score;
-    if (dice == 1 || dice2 == 1) {
+    if (dice == 1) {
       score = 0;
     }
-    else if (dice == dice2) {
-      score = (dice + dice2) * 2;
-    }
-    else {
-      score = dice + dice2;
-    }
 
+    else {
+      score = dice;
+    }
     document.getElementById("currentScore").textContent = score;
-    document.getElementById("dice").textContent = dice;
-    document.getElementById("dice2").textContent = dice2;
+    // document.getElementById("dice").textContent = dice;
+    document.GetElementById("image").src = Dossier+Nom[Numero]
+
 
 }
 
 var currentScore = 0;
 // var score = 0;
 
-document.getElementById("gamePlay").addEventListener("click", gamePlay);
+document.getElementById("roll-dice-btn").addEventListener('click', rollDice());
+// console.log("vous avez obtenu un " + dice " pour un score de " + score);
+randomimage = "images" +dice +".png";
+document.getElementById('dice').src=randomImage;
+// var Dossier = 'images';
+// var Numero = 1;
+// var nom = new Array;
+// Nom[1] = "1.jpg"
+// Nom[2] = "2.jpg"
+// Nom[3] = "3.jpg"
+// Nom[4] = "4.jpg"
+// Nom[5] = "5.jpg"
+// Nom[6] = "6.jpg"
 
-document.getElementById("desactiver").addEventListener("click", function () {
-    document.getElementById("gamePlay").removeEventListener("click", clic);
-});
+
+// document.getElementById("desactiver").addEventListener("click", function () {
+//     document.getElementById("hold-btn").removeEventListener("click", gamePlay());
+// });
 
 
 // var dice = Math.floor(Math.random()*6+1);
